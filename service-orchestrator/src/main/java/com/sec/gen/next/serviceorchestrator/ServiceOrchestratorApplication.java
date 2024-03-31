fce60c90c981f461e577c196c32c2a653c257828
Mateusz Jamróz <jamr.mat@gmail.com>
2024-03-31 21:47:08 +0200

    Started decoupling....

---
+++ service-orchestrator/src/main/java/com/sec/gen/next/serviceorchestrator/ServiceOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.sec.gen.next.serviceorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOrchestratorApplication.class, args);
+    }
+
+}
---
+++ service-orchestrator/src/test/java/com/sec/gen/next/serviceorchestrator/ServiceOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec.gen.next.serviceorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
